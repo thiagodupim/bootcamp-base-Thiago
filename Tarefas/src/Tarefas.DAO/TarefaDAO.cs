ccdc65bb004fd222648a4ce84cb8735a02442361
Thiago Dupim <thiagodupim3@gmail.com>
2023-01-26 16:14:06 -0300

    Implementação de um CRUD no sistema

--- Tarefas/src/Tarefas.DAO/TarefaDAO.cs
+++ Tarefas/src/Tarefas.DAO/TarefaDAO.cs
@@ -4,6 +4,7 @@ using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using Tarefas.DTO;
+using System.Collections.Generic;
 
 namespace Tarefas.DAO
 {
@@ -46,6 +47,55 @@ namespace Tarefas.DAO
                     @"INSERT INTO Tarefa
                     (Titulo, Descricao, Concluida) VALUES
                     (@Titulo, @Descricao, @Concluida);", tarefa
+                );
+            }
+        }
+
+        public List<TarefaDTO> Consultar()
+        {
+            using(var con = Connection)
+            {
+                con.Open();
+                var result = con.Query<TarefaDTO>(
+                    @"SELECT Id, Titulo, Descricao, Concluida FROM Tarefa" 
+                ).ToList();
+                return result;
+            }
+        }
+        public TarefaDTO Consultar(int id)
+        {
+            using(var con = Connection)
+            {
+                con.Open();
+                var result = con.Query<TarefaDTO>(
+                    @"SELECT * FROM Tarefa 
+                    WHERE Id = @Id", new {id} 
+                ).FirstOrDefault();
+                return result;
+            }
+        }
+
+        public void Atualizar(TarefaDTO tarefa)
+        {
+            using (var con = Connection)
+            {
+                con.Open();
+                con.Execute(
+                    @"UPDATE Tarefa
+                    SET Titulo = @Titulo, Descricao = @Descricao, Concluida = @Concluida
+                    WHERE Id = @Id;", tarefa
+                );
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            using (var con = Connection)
+            {
+                con.Open();
+                con.Execute(
+                    @"DELETE FROM Tarefa
+                    WHERE Id = @Id", new{id}
                 );
             }
         }
--- Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -11,22 +11,42 @@ namespace Tarefas.Web.Controllers
 
         public TarefaController()
         {
-            listaDeTarefas = new List<Tarefa>()
-            {
-                new Tarefa() { Id = 1, Titulo = "Escovar os dentes" },
-                new Tarefa() { Id = 2, Titulo = "Arrumar a cama" },
-                new Tarefa() { Id = 3, Titulo = "Por o lixo para fora", Descricao = "somente às terças, quintas e sábados" }
-            };
+    
         }
         
         public IActionResult Details(int id)
         {
-            var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+            var tarefaDAO = new TarefaDAO();
+            var tarefaDTO = tarefaDAO.Consultar(id);
+
+            var tarefa = new Tarefa()
+            {
+                Id = tarefaDTO.Id,
+                Titulo = tarefaDTO.Titulo,
+                Descricao = tarefaDTO.Descricao,
+                Concluida = tarefaDTO.Concluida
+            };
             return View(tarefa);
         }
 
         public IActionResult Index()
-        {            
+        {     
+            var tarefaDAO = new TarefaDAO();
+            var listaDeTarefasDTO = tarefaDAO.Consultar();
+
+            var listaDeTarefas = new List<Tarefa>();
+
+            foreach (var tarefaDTO in listaDeTarefasDTO)
+            {
+                listaDeTarefas.Add(new Tarefa()
+                {
+                    Id = tarefaDTO.Id,
+                    Titulo = tarefaDTO.Titulo,
+                    Descricao = tarefaDTO.Descricao,
+                    Concluida = tarefaDTO.Concluida
+                });
+            }       
+
             return View(listaDeTarefas);
         }
 
@@ -48,7 +68,48 @@ namespace Tarefas.Web.Controllers
             var tarefaDAO = new TarefaDAO();
             tarefaDAO.Criar(tarefaDTO);
 
-            return View();
+            return RedirectToAction("Index");
         }
+        [HttpPost]
+        public IActionResult Update(Tarefa tarefa)
+        {
+            var tarefaDTO = new TarefaDTO 
+            {
+                Id = tarefa.Id,
+                Titulo = tarefa.Titulo,
+                Descricao = tarefa.Descricao,
+                Concluida = tarefa.Concluida
+            };
+
+            var tarefaDAO = new TarefaDAO();
+            tarefaDAO.Atualizar(tarefaDTO);
+
+            return RedirectToAction("Index");
+        }
+     
+        public IActionResult Update(int id)
+        {
+            var tarefaDAO = new TarefaDAO();
+            var tarefaDTO = tarefaDAO.Consultar(id);
+
+            var tarefa = new Tarefa() 
+            {
+                Id = tarefaDTO.Id,
+                Titulo = tarefaDTO.Titulo,
+                Descricao = tarefaDTO.Descricao,
+                Concluida = tarefaDTO.Concluida
+            };
+
+            return View(tarefa);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var tarefaDAO = new TarefaDAO();
+            tarefaDAO.Excluir(id);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
